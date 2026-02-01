3a935c575d49b61b897088ddfd1a82997873f76a
白隐Hakuin <HK256@qq.com>
2026-02-01 18:43:42 +0800

    fix: 修复打包问题

---
+++ web-ext-config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  verbose: true,
+  ignoreFiles: [
+    'icons/Search-Relay.af',
+    'icons/icon1024.png',
+    'web-ext-artifacts/',
+    'docs/',
+    '.gitignore',
+    '.web-ext-ignore',
+    'web-ext-config.cjs'
+  ],
+  build: {
+    overwriteDest: true
+  }
+};
